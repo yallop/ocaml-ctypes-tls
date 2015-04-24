d237edf9ce0e7bd509d503501c8010b874c1eed5
Jeremy Yallop <yallop@gmail.com>
2015-04-24 20:03:33 +0100

    Generate the header.

--- stub-generator/generate.ml
+++ stub-generator/generate.ml
@@ -6,6 +6,7 @@ let generate (module T : Tls_types.TLS) dirname =
   let path basename = Filename.concat dirname basename in
   let ml_fd = open_out (path "tls_generated_bindings.ml") in
   let c_fd = open_out (path "tls.c") in
+  let h_fd = open_out (path "tls_generated.h") in
   let stubs = (module Tls_bindings.Stubs(T) : Cstubs_inverted.BINDINGS) in
   begin
     (* Generate the ML module that links in the generated C. *)
@@ -14,9 +15,20 @@ let generate (module T : Tls_types.TLS) dirname =
 
     (* Generate the C source file that exports OCaml functions. *)
     Format.fprintf (Format.formatter_of_out_channel c_fd)
-      "#define const@\n#include \"tls.h\"@\n%a"
+      "#define const@\n#include \"tls.h\"@\n#include \"tls_generated.h\"@\n%a"
       (Cstubs_inverted.write_c ~prefix) stubs;
+
+    (* Generate a header. *)
+    Format.fprintf (Format.formatter_of_out_channel h_fd)
+      "%(%)%(%)%(%)%(%)%a@\n%(%)@."
+      "#ifndef TLS_GENERATED_H@\n"
+      "#define TLS_GENERATED_H@\n@\n"
+      "#include <stdint.h>@\n"
+      "#include <stddef.h>@\n@\n"
+      (Cstubs_inverted.write_c_header ~prefix) stubs
+      "#endif /* TLS_GENERATED_H */"
   end;
+  close_out h_fd;
   close_out c_fd;
   close_out ml_fd
 
